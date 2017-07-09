647f703bd8ddd6e86a4d984690bd22f5048b07dd
Koki Mitsunami <k.mitsunami@gmail.com>
2017-07-09 06:54:29 -0700

    Exercise-1 Done

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -19,8 +19,8 @@ vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
 
 # Call the filter function to obtain the resultant downsampled point cloud
 cloud_filtered = vox.filter()
-filename = 'voxel_downsampled.pcd'
-pcl.save(cloud_filtered, filename)
+#filename = 'voxel_downsampled.pcd'
+#pcl.save(cloud_filtered, filename)
 
 
 ### PassThrough filter
@@ -36,28 +36,46 @@ passthrough.set_filter_limits (axis_min, axis_max)
 
 # Finally use the filter function to obtain the resultant point cloud. 
 cloud_filtered = passthrough.filter()
-filename = 'pass_through_filtered.pcd'
-pcl.save(cloud_filtered, filename)
+#filename = 'pass_through_filtered.pcd'
+#pcl.save(cloud_filtered, filename)
 
 
 ### RANSAC plane segmentation
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
+
+# Set the model you wish to fit 
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+# Experiment with different values for max_distance 
+# for segmenting the table
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
 
 
 ### Extract inliers
-
-# Save pcd for table
-# pcl.save(cloud, filename)
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+#filename = 'extracted_inliers.pcd'
+#pcl.save(extracted_inliers, filename)
 
 
 ### Extract outliers
+extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+#filename = 'extracted_outliers.pcd'
+#pcl.save(extracted_outliers, filename)
 
 
 ### Save pcd for tabletop objects
+#Table.pcd - Containing only the points that belong to the table
+filename = 'table.pcd'
+pcl.save(extracted_inliers, filename)
 
-
-    #Table.pcd - Containing only the points that belong to the table
-    # example: filename = 'table.pcd' for table
-    #pcl.save(cloud, 'table.pcd')
-
-    #Objects.pcd - Containing all the tabletop objects
+#Objects.pcd - Containing all the tabletop objects
+filename = 'objects.pcd'
+pcl.save(extracted_outliers, filename)
 
